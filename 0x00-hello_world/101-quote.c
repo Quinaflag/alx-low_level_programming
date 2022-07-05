b965944e9f390598049dafbc7a90e1a96589d1dc
Quinaflag <onuohaqueen14@gmail.com>
2022-07-04 21:33:55 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-putchar(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),
 return (0);
 }
