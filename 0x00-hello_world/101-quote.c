f42b35aa02be02caecee8ddb3878e46f39026bf5
Quinaflag <onuohaqueen14@gmail.com>
2022-07-04 22:06:39 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main - prints out the last part of the quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-((putchar'("and that piece of art is useful\"- Dora Korpar, 2015-10-19"\n)'));
-return (0);
+    write(2 "and that piece of art is useful\"- Dora Korpar, 2015-10-19"\n, 58);
+    return (1);
 }
