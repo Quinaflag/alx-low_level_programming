65c4bc43414ac4e51024a22ba03117a348b95e61
Quinaflag <onuohaqueen14@gmail.com>
2022-07-05 21:23:21 -0700

    Task 0 - 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+int i;
+for (i = 10; 1 < 20; i++)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n')
+return (0);
+}
