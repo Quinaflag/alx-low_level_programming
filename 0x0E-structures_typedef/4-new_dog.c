791c6f92381b1a7cd96fb21a8e3bace7ed52c419
Quinaflag <onuohaqueen14@gmail.com>
2022-08-01 21:51:12 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,11 +30,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	n_dog->name = malloc(lenN + 1);
 	if (n_dog->name == NULL)
 	{
-		free(n-dog->name);
+		free(n_dog->name);
 		free(n_dog);
 		return (NULL);
 	}
-	n_dog->owner == (NULL)
+	n_dog->owner = malloc(lenO + 1);
+	if (n_dog->owner == NULL)
 	{
 		free(n_dog->name);
 		free(n_dog->owner);
