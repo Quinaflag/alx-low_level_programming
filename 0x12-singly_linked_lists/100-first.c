615e2f5f1d1d7198787393bfae52c2a7cc303231
Quinaflag <106886256+Quinaflag@users.noreply.github.com>
2022-08-11 16:28:10 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,12 @@
-#include "lists.h"
+#include <stdio.h>
+
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: The number of nodes.
+ * bmain - function executed before main
+ * Return: no return.
  */
 
-size_t print_list(const list_t *h)
+void __attribute__ ((constructor)) bmain()
 {
-	size_t elem;
-
-	elem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		elem++;
-	}
-	return (elem);
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
